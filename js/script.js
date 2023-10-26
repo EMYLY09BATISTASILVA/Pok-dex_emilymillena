aa26585e9ef0106556c29e8ce294435f36d45393
EMYLY09BATISTASILVA <emilybsilva09@gmail.com>
2023-10-26 15:52:19 -0300

    novooo

--- js/script.js
+++ js/script.js
@@ -54,3 +54,116 @@ document.addEventListener("DOMContentLoaded", function() {
     // Adicionar evento de clique ao botão de pesquisa
     searchButton.addEventListener("click", searchPokemonByName);
 });
+
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+
+let searchPokemon = 1;
+
+
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const searchButton = document.getElementById("searchButton");
+    const input = document.getElementById("searchInput");
+    const pokemonDetails = document.querySelector('.pokemon__details');
+
+    async function fetchPokemonData(pokemon) {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Erro ao buscar dados dos Pokémon: " + error);
+        }
+    }
+
+    async function renderPokemonDetails(pokemon) {
+        const data = await fetchPokemonData(pokemon);
+
+        if (data) {
+            pokemonDetails.innerHTML = `
+                <h2>${data.name}</h2>
+                <p>Height: ${data.height}</p>
+                <p>Weight: ${data.weight}</p>
+                <p>Types: ${data.types.map(type => type.type.name).join(', ')}</p>
+                <p>Abilities: ${data.abilities.map(ability => ability.ability.name).join(', ')}</p>
+                <p>Moves: ${data.moves.length}</p>
+            `;
+        } else {
+            pokemonDetails.innerHTML = '<p>Pokemon not found.</p>';
+        }
+    }
+
+    searchButton.addEventListener("click", function() {
+        const searchTerm = input.value.toLowerCase();
+        renderPokemonDetails(searchTerm);
+    });
+});
+
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not found :c';
+    pokemonNumber.innerHTML = '';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderAllPokemonDetails(input.value.toLowerCase());
+});
+
+
+
+
+renderPokemon(searchPokemon);
+
+
+
